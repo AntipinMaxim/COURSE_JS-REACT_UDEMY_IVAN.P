6630596e948bac298364a46dea66201e133ffa64
Maxim <activisionmax@gmail.com>
2024-11-20 11:59:56 +0700

    изучение рекурсии

--- project/js/script.js
+++ project/js/script.js
@@ -21,6 +21,35 @@ const movieDB = {
         "Ла-ла лэнд",
         "Одержимость",
         "Скотт Пилигрим против..."
-    ]
+    ],
 };
 
+const advBlocks = document.querySelectorAll('.promo__adv img'),
+      promoBg = document.querySelector('.promo__bg'),  
+      genre = promoBg.querySelector('.promo__genre'),
+      movieList = document.querySelector('.promo__interactive-list')
+
+advBlocks.forEach(item => item.remove());
+
+genre.textContent = 'драма';
+
+promoBg.style.backgroundImage = 'url(img/bg.jpg)';
+
+movieList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}) ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+    console.log('test');
+});
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', () => {
+    alert('click')
+}); 
---
+++ recursion/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let studends = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        },{
+            name: 'Anna',
+            progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
+
+function totalProgressStudends(data){
+    if(Array.isArray(data)) {
+        let total = 0;
+
+        for(let i = 0; i < data.length; i++){
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+
+    } else {
+        let total = [0, 0];
+
+        for(let subData of Object.values(data)){
+            const subDataArr = totalProgressStudends(subData);
+            total[0] += subDataArr[0];
+            total[1] += subDataArr[1];
+        }
+        return total;
+    }
+};
+
+console.log(totalProgressStudends(studends)[0]/totalProgressStudends(studends)[1]);
