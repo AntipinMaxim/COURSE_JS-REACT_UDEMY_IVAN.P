31f652a7578dad34c96206814d07aab294565546
Maxim <activisionmax@gmail.com>
2024-11-27 15:47:10 +0700

    добавил функционал модального окна

---
+++ Date/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const date = new Date();
+const newData = new Date(date.getTime());
+
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+
+console.log(date.getTime());
+
+
+
+
+
+
+
+
--- Food/js/script.js
+++ Food/js/script.js
@@ -2,6 +2,8 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 
+    //TABS
+
     const tabsContent = document.querySelectorAll('.tabcontent'),
           tabsWrapper = document.querySelector('.tabheader__items'),
           tabs = tabsWrapper.querySelectorAll('.tabheader__item');
@@ -41,5 +43,122 @@ window.addEventListener('DOMContentLoaded', () => {
     hideTabs();
     showTabs();
 
-   
+
+    //Timer
+
+    const deadline = '2024-11-28';
+
+    const getTimerRemaining = (endtime) => {
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - new Date();
+
+        if(t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), 
+            hours = Math.floor(t / (1000 * 60 * 60) % 24),
+            minutes = Math.floor(t / (1000 * 60 ) % 60),
+            seconds = Math.floor(t / 1000 % 60);
+        }
+
+        return{
+            t,
+            days,
+            hours,
+            minutes,
+            seconds
+        }       
+    }
+
+    function getZero(num){
+        if(num < 10) {
+            num = `0${num}`;
+        }
+        return num;
+    }
+
+    const setTimer = (selector, endtime) => {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),    
+              timeInterval = setInterval(updateTimer, 1000);
+        
+        updateTimer();
+
+        function updateTimer() {
+            const total = getTimerRemaining(endtime);
+
+            days.textContent = getZero(total.days);
+            hours.textContent = getZero(total.hours);
+            minutes.textContent = getZero(total.minutes);
+            seconds.textContent = getZero(total.seconds);
+
+            if(total.t <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+
+    setTimer('.timer', deadline);
+
+
+    // Modal
+    const modalCloseBtn = document.querySelector('.modal__close'),
+          modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          timerModal = setTimeout(showModal, 5000);
+
+    function showModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+
+        document.body.style.overflow = 'hidden';
+
+        clearTimeout(timerModal);
+    };
+
+    const closeModal = () => {
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+
+        document.body.style.overflow = '';
+    };
+
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () =>{
+            showModal();
+        });
+    });
+
+    modalCloseBtn.addEventListener('click', () => {
+        closeModal();
+    });
+
+    modal.addEventListener('click', (e) => {
+        if(e.target === modal){
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape' && modal.classList.contains('show')){
+            closeModal();
+        }
+    });
+
+    function showModalByScroll() {
+        if(window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight){
+            showModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        } 
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
 });
